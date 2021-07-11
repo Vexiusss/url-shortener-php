d9f4f548614cc9d8430b1f450d5fa9e48cfa7424
GetGaming <75634692+getgaming@users.noreply.github.com>
2021-07-11 16:02:19 +0200

    Initial commit

---
+++ gen.php
@@ -0,0 +1,19 @@
+<?php
+$link = htmlspecialchars($_POST['url']);
+$end = htmlspecialchars($_POST['gen']);
+$myfile = fopen('page/' . $end . '.html', 'w') or die("ujsciusdnfiks");
+
+$txt = '
+<html><head><meta http-equiv="refresh" content="3;url="'. $link .'"</head><body>redirecting</body></html>
+';
+fwrite($myfile, $txt);
+fclose($myfile);
+
+$myfile1 = fopen('copy/' . $end . '.html', 'w') or die("ujsciusdnfiks");
+
+$txt1 = '
+<html><head></head><body><p>localhost/challenge/page'. $end .'.html</p></body></html>
+';
+fwrite($myfile1, $txt1);
+fclose($myfile1);
+header('Location: copy/'. $end .'.html');
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
+    </head>
+    <body>
+
+    <center>
+        <form action="gen.php" method="POST">
+            <input name="url" type="text" placeholder="url">
+            <input name="gen" type="text" placeholder="gen">
+            <input type="submit">
+        </form>
+    </center>
+
+
+
+    </body>
+</html>
